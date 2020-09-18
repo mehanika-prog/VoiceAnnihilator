a85a9ca73c266350436fd948826d5609c65f1dae
Bohdan Mandryk <bohdan.mandryk.2001@gmail.com>
2020-09-18 20:44:25 +0300

    first try of work with audio

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
+const path = require('path')
+const fs = require('fs')
+const url = require('url')
+const https = require('https')
+const config = require('config')
 const TelegramBot = require('node-telegram-bot-api')
+const speech = require('@google-cloud/speech')
 
-const token = '1384093348:AAEheFAHfeFQu-goUxKTq0qOI0WVZSZZjuU'
+//=================================//
+
+const voicePath = path.join(__dirname, '/voices')
+
+const token = config.get('botToken')
+
+//=================================//
 
 const bot = new TelegramBot(token, {polling: true})
 
+const client = new speech.SpeechClient();
+
+//=================================//
+
 bot.on('voice', msg => {
 
-    const chatId = msg.chat.id
-    const messageId = msg.message_id
-    bot.deleteMessage(chatId, messageId)
-    bot.sendSticker(chatId, 'CAACAgIAAx0CRKn0SgACoh5fXhZghfXoKBKacdFkLirxFaUSPAACSQIAAladvQoqlwydCFMhDhsE')
+	const chatId = msg.chat.id
+	const messageId = msg.message_id
+
+	bot.deleteMessage(chatId, messageId)
+	bot.sendSticker(chatId, 'CAACAgIAAx0CRKn0SgACoh5fXhZghfXoKBKacdFkLirxFaUSPAACSQIAAladvQoqlwydCFMhDhsE')
+
+	bot.downloadFile(msg.voice.file_id, voicePath)
+	.then(async fileName => {
+		
+		const file = fs.readFileSync(fileName)
+		// fs.unlink(fileName, () => {})
+		const audioBytes = file.toString('base64')
+		
+		const request = {
+
+			config: {
+				encoding: "LINEAR16",
+				sampleRateHertz: 16000,
+				languageCode: "uk-UA",
+			},
+			audio: {
+				content: audioBytes
+			}
+
+		}
+
+		const [response] = await client.recognize(request)
+		const transcription = response.results
+			.map(result => result.alternatives[0].transcript)
+			.join('\n')
+
+		// console.log([response])
+	
+	})
 
 })
 
