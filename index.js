f52f2a44dfd4bf4172a6addd1589eb8ef4aac7f3
Ubuntu <ubuntu@ip-172-31-8-203.us-east-2.compute.internal>
2020-09-19 17:02:42 +0000

    trying to add settings

--- index.js
+++ index.js
@@ -36,10 +36,11 @@ bot.onText(/\/start/, msg => {
 
 		switch (language_code) {
 
-			case 'en': language_code = 'en-US'
-			case 'sk': language_code = 'sk-SK'
-			case 'ru': language_code = 'ru-RU'
-			case 'uk': language_code = 'uk-UA'
+			case 'en': language_code = 'en-US'; break
+			case 'sk': language_code = 'sk-SK'; break
+			case 'ru': language_code = 'ru-RU'; break
+			case 'uk': language_code = 'uk-UA'; break
+			default: language_code = 'ru-RU'; break
 
 		}
 
@@ -47,7 +48,7 @@ bot.onText(/\/start/, msg => {
 
 			if (row) {
 
-				console.log(row);
+				
 
 			}else{
 
@@ -71,43 +72,107 @@ bot.onText(/\/start/, msg => {
 
 })
 
+bot.onText(/\/cl/, msg => {
+
+	if (msg.chat.type === 'private') {
+
+		const chatId = msg.from.id
+		// const is_bot = msg.from.is_bot
+		// const first_name = msg.from.first_name
+		// const last_name = msg.from.last_name
+		// const username = msg.from.username
+		// let language_code = msg.from.language_code
+
+		bot.sendMessage(chatId, 'Please select a language.', {
+
+			reply_markup: {
+
+				inline_keyboard: [
+
+					{
+						text: 'English',
+						callback_data: 'en-US'	
+					},
+					{
+						text: 'Slovenčina',
+						callback_data: 'sk-SK'	
+					},
+					{
+						text: 'Русский',
+						callback_data: 'ru-RU'	
+					},
+					{
+						text: 'Українська',
+						callback_data: 'uk-UA'	
+					}
+
+				]
+
+			}
+
+		})
+
+	}
+
+})
+
 bot.on('voice', msg => {
 
+	const user_id = msg.from.id
 	const chatId = msg.chat.id
 	const messageId = msg.message_id
 
 	bot.deleteMessage(chatId, messageId)
-	bot.sendSticker(chatId, 'CAACAgIAAx0CRKn0SgACoh5fXhZghfXoKBKacdFkLirxFaUSPAACSQIAAladvQoqlwydCFMhDhsE')
 
-	// bot.downloadFile(msg.voice.file_id, voicePath)
-	// .then(async fileName => {
-		
-	// 	const file = fs.readFileSync(fileName)
-	// 	const audioBytes = file.toString('base64')
-		
-	// 	const request = {
+	db.get('SELECT voice_to_text, language_code FROM Users WHERE id = ?', user_id, (err, row) => {
 
-	// 		config: {
-	// 			encoding: "OGG_OPUS",
-	// 			sampleRateHertz: 16000,
-	// 			languageCode: "uk-UA",
-	// 		},
-	// 		audio: {
-	// 			content: audioBytes
-	// 		}
+		if (row){
 
-	// 	}
+			if (row.voice_to_text) {
 
-	// 	const [response] = await client.recognize(request)
-	// 	const transcription = response.results
-	// 		.map(result => result.alternatives[0].transcript)
-	// 		.join('\n')
+				bot.downloadFile(msg.voice.file_id, voicePath)
+				.then(async fileName => {
+					
+					const file = fs.readFileSync(fileName)
+					const audioBytes = file.toString('base64')
+					
+					const request = {
+			
+						config: {
+							encoding: "OGG_OPUS",
+							sampleRateHertz: 16000,
+							languageCode: row.language_code,
+						},
+						audio: {
+							content: audioBytes
+						}
+			
+					}
+			
+					const [response] = await client.recognize(request)
+					const transcription = response.results
+						.map(result => result.alternatives[0].transcript)
+						.join('\n')
+			
+					bot.sendMessage(chatId, `${msg.from.first_name} ${msg.from.last_name} say:\n\n${transcription}`)
+			
+					fs.unlink(fileName, () => {})
+				
+				})
 
-	// 	bot.sendMessage(chatId, `${msg.from.first_name} ${msg.from.last_name} say:\n\n${transcription}`)
-
-	// 	fs.unlink(fileName, () => {})
+			}else{
 	
-	// })
+				bot.sendSticker(chatId, 'CAACAgIAAx0CRKn0SgACoh5fXhZghfXoKBKacdFkLirxFaUSPAACSQIAAladvQoqlwydCFMhDhsE')
+	
+			}
+
+		}else{
+	
+			bot.sendSticker(chatId, 'CAACAgIAAx0CRKn0SgACoh5fXhZghfXoKBKacdFkLirxFaUSPAACSQIAAladvQoqlwydCFMhDhsE')
+
+		}
+
+	})
 
 })
 
